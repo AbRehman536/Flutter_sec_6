6bf17de0db8e502cc3ad7079944909ae543afa69
Abdullah <abdullahrehman1019@gmail.com>
2025-09-01 13:33:55 -0600

    Images done

---
+++ lib/asset_image.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AssetImageDemo extends StatelessWidget {
+  const AssetImageDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("Assets Image"),
+      ),
+      body: Column(children: [
+        Image.asset("assets/images/download.jpg",width: 500,height: 300,),
+      ],),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sec_6/asset_image.dart';
 import 'package:flutter_sec_6/fab.dart';
 import 'package:flutter_sec_6/nested_row_column.dart';
+import 'package:flutter_sec_6/network_image.dart';
 import 'package:flutter_sec_6/row_column.dart';
 
 import 'appbardemo.dart';
@@ -36,7 +38,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: NestedRowColumn(),
+      home: AssetImageDemo(),
     );
   }
 }
